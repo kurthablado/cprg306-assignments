9d02df8a76e92b11145450554db9ad054bb13946
kurthablado <kurt.hablado@edu.sait.ca>
2024-05-21 08:51:51 -0600

    Update page.js
    Assignment 2 - Part 3

--- app/page.js
+++ app/page.js
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <p>Hello World!</p>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
       
       <div>
         <Link href="/week-2">Week 2</Link>
       </div>
-      
+
     </main>
   );
 }
