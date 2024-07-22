da8aabbb0b433bbe813f2d0fb6b4bb14a37dc6f0
kurthablado <kurt.hablado@edu.sait.ca>
2024-07-22 13:37:21 -0600

    Update firebase.js

--- app/week-8/_utils/firebase.js
+++ app/week-8/_utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
  
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCCz9ZZ8Whf6kYRCjd-1anqcaFMrgmGygE",
-    authDomain: "cprg306-assignments-c0e24.firebaseapp.com",
-    projectId: "cprg306-assignments-c0e24",
-    storageBucket: "cprg306-assignments-c0e24.appspot.com",
-    messagingSenderId: "844137488499",
-    appId: "1:844137488499:web:548a9590c4526219cb0a06",
-    measurementId: "G-71J18JTQ2L"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,   
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
  
 // Initialize Firebase
